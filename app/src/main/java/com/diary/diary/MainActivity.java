6821caaf468b804022b99129ff19a49617407c26
Megii <magdalena.wojciechowska@poczta.onet.pl>
2017-01-28 00:49:04 +0100

    and

---
+++ app/src/main/java/com/diary/diary/MainActivity.java
@@ -0,0 +1,33 @@
+package com.diary.diary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button  zadanie=(Button)findViewById(R.id.Bt);
+        Button  zdjecie=(Button)findViewById(R.id.Zdj);
+
+        zadanie.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent int1= new Intent(MainActivity.this,Post.class);
+                startActivity(int1);
+            }
+        });
+
+      /*  zdjecie.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent int2= new Intent(MainActivity.this,Photo.class);
+                startActivity(int2);
+            }
+        });*/
+    }
+
+}
---
+++ app/src/main/java/com/diary/diary/Photo.java
@@ -0,0 +1,18 @@
+package com.diary.diary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by magda on 27.01.2017.
+ */
+
+public class Photo extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+
+
+}
